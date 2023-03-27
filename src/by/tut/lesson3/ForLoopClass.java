4026a4036db5bc168cda093fd1dd65ae813b5f85
Макс <mtrayanovicg2@gmail.com>
2023-03-27 21:25:33 +0300

    forLesson3

---
+++ src/by/tut/lesson3/ForLoopClass.java
@@ -0,0 +1,32 @@
+package by.tut.lesson3;
+
+public class ForLoopClass {
+    public static void main(String[] args) {
+//        for (int counter = 0; counter < 100; counter++) {
+//            System.out.println("" + counter);
+        {
+//            for (int x = 0; x < 100; x++) {
+//                if (x % 2 == 0) {
+//                    System.out.println(x);
+//                }
+//
+//                int count = 100;
+//                while (count > 0) {
+//                    System.out.println("count:" + count--);
+//                }
+//                do {
+//                    System.out.println("count again:" + count++);
+//                }
+//                while (count < 100);
+//            }
+            int res = 0;
+            while (res > 0) {
+                System.out.println("res:" + res++);
+            }
+            do {
+                System.out.println("res" + res++);
+                res += 1;
+            }while (res<100);
+        }
+    }
+}
