d7a84c38bea622898373570b84c417887eaa2bee
Макс <mtrayanovicg2@gmail.com>
2023-03-27 21:44:04 +0300

    RandomLesson3

---
+++ src/by/tut/lesson3/RandomExample.java
@@ -0,0 +1,12 @@
+package by.tut.lesson3;
+
+import java.util.Random;
+
+public class RandomExample {
+    public static void main(String[] args) {
+        Random random = new Random(5);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(random.nextInt(5));
+        }
+    }
+}
