e7eb4e7d4d68e59d1ba73a7f55c1275ee58542a9
Lesson4 <mtrauanovich2@gmail.com>
2023-04-02 20:29:09 +0300

    Homework4/Parts1

---
+++ src/by/tut/Homework/Parts4.java
@@ -0,0 +1,18 @@
+package by.tut.Homework;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Parts4 {
+    public static void main(String[] args) {
+        int[] a = new int[7];
+        int i=0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Massive size:");
+        for (int element : a) {
+            System.out.println(a[i]  + " - ");
+            i++;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
